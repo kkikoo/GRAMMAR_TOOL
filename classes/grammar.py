3f6eb27d28346c0b96dab2684d28a29bd658e4f4
Shiyi Mu <smu2@uci.edu>
2022-11-28 15:11:02 -0800

    create grammar.py

---
+++ classes/grammar.py
@@ -0,0 +1,13 @@
+# grammar.py
+# We defined a class called Grammar
+
+# The Grammar class records all variable names and rules in the file
+# * and combine them as a dictionary, grammar_map
+# * the key of the map is Class Variable
+# * the value of the map is Class Rule
+
+# We also define a function: generate_sentence(start_variable, number_of_sentences) function
+# * which accepts the name of start_variable,
+# * the number of sentences expected to be output,
+# * and then generates a series of sentences according to the rules
+
