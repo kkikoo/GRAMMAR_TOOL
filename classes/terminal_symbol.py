31435e1a325facd02e3dca4e83dfbbfd096135cc
Shiyi Mu <smu2@uci.edu>
2022-11-28 15:39:06 -0800

    create terminal_symbol.py

---
+++ classes/terminal_symbol.py
@@ -0,0 +1,8 @@
+# terminal_symbol.py
+# We defined a class called Terminal
+
+# a Terminal is just a string, here we rewrite the __hash__() method for this class
+
+# We also define a function: generate_sentence(grammar_map) function
+# * which accepts the grammar_map of file,
+# * and then return the var_name in this class
