5640b58ef48bdbec0383ca45625a2c1f556aeb0f
Shiyi Mu <smu2@uci.edu>
2022-11-28 15:29:10 -0800

    create grammar.py class Grammar

--- classes/grammar.py
+++ classes/grammar.py
@@ -11,3 +11,23 @@
 # * the number of sentences expected to be output,
 # * and then generates a series of sentences according to the rules
 
+class Grammar:
+    def __init__(self, map_of_grammar: dict) -> None:
+        self.grammar_map = map_of_grammar
+
+    def generate_sentence(self, start_variable: str, number_of_sentences: int):
+        """generate sentence from grammar, with given start_variable and number_of_sentences
+        Args:
+            start_variable (str): the name of start_variable, such as "HowIsBoo"
+            number_of_sentences (int): number_of_sentences, such as 10
+        """
+        try:
+            rules = self.grammar_map[start_variable]
+            for i in range(number_of_sentences):
+                print(
+                    rules.generate_sentence(self.grammar_map)
+                )
+        except:
+            raise ValueError(
+                "Do not have a variable named {}".format(start_variable))
+
